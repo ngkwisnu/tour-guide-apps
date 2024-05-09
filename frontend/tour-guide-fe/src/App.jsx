c28e8b5e0cb49f0dce0736c51214209a29145a85
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-05-09 11:14:50 +0800

    Register Page include Tour Guide and Users Register

--- frontend/tour-guide-fe/src/App.jsx
+++ frontend/tour-guide-fe/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
-import Login from './component/Login'
 import Register from './component/Register'
+import TourGuideRegister from './component/sub-component/TourGuideRegister'
+import UsersRegister from './component/sub-component/UsersRegister'
 
 function App() {
   const [count, setCount] = useState(0)
--- frontend/tour-guide-fe/src/component/sub-component/UsersRegister.jsx
+++ frontend/tour-guide-fe/src/component/sub-component/UsersRegister.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Register = () => {
+const UsersRegister = () => {
   return (
     <div className='container mx-auto'>
       <div className='flex justify-center bg-cover h-screen bg-slate-100 relative'>
@@ -14,11 +14,14 @@ const Register = () => {
                 <button className='bg-transparent border border-orange-300 rounded-full h-10 w-4/5'>Sign in</button>
               </div>
               <div className='flex flex-col items-center gap-10 lg:w-4/5 lg:my-auto lg:gap-8 px-16 py-16'>
-                <h1 className='text-2xl text-slate-800 text-center font-bold'>Create Account</h1>
-                <p className='text-center text-sm'>Anda akan masuk sebagai?</p>
-                <button className='bg-transparent border bg-orange-400 rounded-full w-4/5 h-10 font-semibold text-white'>Tour Guide</button>
-                <p>atau</p>
-                <button className='bg-transparent border bg-orange-400 rounded-full  w-4/5 h-10 font-semibold text-white'>Wisatawan</button>
+                <h1 className='text-xl text-slate-800 text-center font-bold'>Create Users Account</h1>
+                <div class="w-full flex flex-col gap-4 items-center">
+                    <input type="hidden" id="base-input" className="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5" value='user'/>
+                    <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5" placeholder='Nama Lengkap'/>
+                    <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5" placeholder='Email'/>
+                    <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-sm rounded-lg block w-full p-2.5" placeholder='Password'/>
+                    <button className='bg-transparent border bg-orange-400 text-white font-bold rounded-full h-10 w-1/2 my-3'>Sign in</button>
+                </div>
               </div>
           </div>
       </div>
@@ -26,4 +29,4 @@ const Register = () => {
   )
 }
 
-export default Register
+export default UsersRegister
