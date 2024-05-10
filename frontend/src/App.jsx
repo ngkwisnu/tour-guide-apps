dd2eea2cce3b0376a7d73aad9a4b80ffde53fdd1
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-05-10 21:09:55 +0800

    Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import CreateAccount from './pages/CreateAccount'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Register from './pages/Register'
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,27 +2,84 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='container mx-auto'>
-      <div className='flex justify-center bg-cover h-screen bg-slate-100 relative'>
-          <img src="assets/img/lingkaran.png" alt="" className='absolute z-10 bottom-0 left-0'/>
-          <img src="assets/img/segitiga.png" alt="" className='absolute z-10 top-0 right-0'/>
-          <div className='h-1/2 lg:h-4/5 bg-gradient-to-tr from-orange-400/30 to-cyan-400/30 m-auto rounded-lg border-2 border-white backdrop-blur-lg z-20 flex shadow-2xl'>
-              {/* <div className='backdrop-blur-sm bg-white/60 w-2/5 rounded-e-lg py-32 px-20 flex flex-col items-center gap-10 p shadow-lg'> */}
-              <div className='backdrop-blur-sm bg-white/60 w-2/5 rounded-e-lg py-32 px-10 lg:flex flex-col items-center gap-10 p shadow-lg hidden'>
-                <h1 className='text-xl text-center'>Wellcome Back!</h1>
-                <p className='text-center text-sm font-light'>To keep connected with us please login with your personal info</p>
-                <button className='bg-transparent border border-orange-300 rounded-full h-10 w-4/5'>Sign in</button>
-              </div>
-              <div className='flex flex-col items-center gap-10 lg:w-4/5 lg:my-auto lg:gap-8 px-16 py-16'>
-                <h1 className='text-2xl text-slate-800 text-center font-bold'>Create Account</h1>
-                <p className='text-center text-sm'>Anda akan masuk sebagai?</p>
-                <button className='bg-transparent border bg-orange-300 rounded-full w-4/5 h-10 font-semibold text-white'>Tour Guide</button>
-                <p>atau</p>
-                <button className='bg-transparent border bg-orange-300 rounded-full  w-4/5 h-10 font-semibold text-white'>Wisatawan</button>
-              </div>
-          </div>
-      </div>
-    </div>
+    <>
+        <div className='mx-auto lg:flex lg:h-auto'>
+            <div className='hidden lg:flex lg:h-screen w-3/5 bg-cover h-auto' style={{ backgroundImage: 'url("assets/img/farm.png")' }}>
+                <div className='lg:w-1/5 lg:h-1/5 lg:ml-10 lg:mt-5'>
+                    <img src="assets/img/logo-white.png" alt="" />
+                </div>
+            </div>
+            <div className='h-screen lg:h-auto content-center lg:w-2/5 lg:py-16'>
+                <h1 className='text-2xl text-slate-700 font-semibold text-center mb-5'>Daftar</h1>
+                <div className='w-full flex flex-col items-center lg:hidden'>
+                    <form className="w-4/5 mx-auto">
+                        <div className="mb-3">
+                            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Nama</label>
+                            <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                            <input type="email" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">No. Telepon / Whatsapp</label>
+                            <input type="number" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Kata Sandi</label>
+                            <input type="password" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                        </div>
+                        <div className="mb-9">
+                            <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900">Konfirmasi Kata Sandi</label>
+                            <input type="password" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required/>
+                        </div>
+                        <div className='flex justify-center'>
+                            <button type='submit' className='w-2/5 bg-sky-300 rounded-full h-10 text-white font-semibold'>Mulai</button>
+                        </div>
+                    </form>
+                    <div className='flex gap-3 mt-10 justify-center'>
+                        <p className='text-sm content-center'>Get The App Now!</p>
+                        <img src="assets/img/googleplay.png" className='w-1/3' alt="" />
+                    </div>
+                    <p className='mt-10'>Sudah punya akun? <span className='text-sky-300'>Masuk</span></p>
+                </div>
+                <div className='w-full hidden flex-col items-center lg:flex'>
+                    <form className="w-4/5 mx-auto">
+                        <div className="mb-3">
+                            <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Nama</label>
+                            <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-full bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 " required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                            <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-full bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 " required/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">No. Telepon / Whatsapp</label>
+                            <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-full bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 " required/>
+                        </div>
+                        <div className='hidden lg:flex justify-between gap-4 w-full'>
+                            <div className="mb-3 w-1/2">
+                                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Kata Sandi</label>
+                                <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-full bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 " required/>
+                            </div>
+                            <div className="mb-3 w-1/2">
+                                <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900">Konfirmasi Kata Sandi</label>
+                                <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-full bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 " required/>
+                            </div>
+                        </div>
+                        <div className='flex justify-center lg:mt-8'>
+                            <button type='submit' className='w-1/5 bg-sky-300 rounded-full h-8 text-white font-semibold'>Mulai</button>
+                        </div>
+                    </form>
+                    <div className='flex gap-3 mt-10 justify-center lg:hidden'>
+                        <p className='text-sm content-center'>Get The App Now!</p>
+                        <img src="assets/img/googleplay.png" className='w-1/3' alt="" />
+                    </div>
+                    <p className='mt-10 lg:mt-7'>Sudah punya akun? <span className='text-sky-300'>Masuk</span></p>
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
