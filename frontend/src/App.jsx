010fab46e30f591ddce9c5f186b5355284229fcf
melllonn07 <maya.anggita713@gmail.com>
2024-05-16 12:00:37 +0700

    adding profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Register from './pages/Register'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Register/>} />
-          <Route path='/register-tour-guide' element={<TourGuideRegister/>} />
-          <Route path='/register-users' element={<UsersRegister/>} />
-        </Routes>
-      </BrowserRouter> */}
-      {/* <CreateAccount/> */}
-      <Register/>
-    </>
+    <div>
+      
+    </div>
   )
 }
-
-export default App
