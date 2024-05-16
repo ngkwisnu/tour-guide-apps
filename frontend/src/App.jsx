4f0d880bef1dc495867cbaa4af5a1f10fd8da243
melllonn07 <maya.anggita713@gmail.com>
2024-05-16 12:45:32 +0700

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import ProfileAdmin from './pages/ProfileAdmin';
 
 export default function App() {
   return (
-    <div>
-      kasdasda
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/profile-admin" element={<ProfileAdmin />}/>
+        <Route path="/pesanan-admin" element={<ProfileAdmin />}/>
+        <Route path="/kelola-paket" element={<ProfileAdmin />}/>
+        <Route path="/riwayat" element={<ProfileAdmin />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ frontend/src/pages/ProfileAdmin.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function ProfileAdmin() {
+  return (
+    <>
+    <div className='mx-[72px]'>
+    <header className='h-[93px] bg-red-500'>
+        <nav>
+            <h1>logo</h1>
+            <ul>
+                <li>Home</li>
+                <li>App</li>
+                <li>Wisata</li>
+                <li>Notifikasi</li>
+                <li>Akun</li>
+            </ul>
+        </nav>
+      </header>
+    </div>
+      
+    </>
+  )
+}
