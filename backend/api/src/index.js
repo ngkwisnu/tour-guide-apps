a7fca69c239cfc07fb85882f282b9dbd304907ce
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-05-26 13:50:55 +0800

    Testing API & Publish use Postman

--- backend/api/src/index.js
+++ backend/api/src/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const PORT = 5000;
+const PORT = 3000;
 const HOST = '0.0.0.0';
 const express = require('express');
 const app = express();
@@ -9,6 +9,12 @@ const server = http.createServer(app);
 const dotenv = require('dotenv');
 // Panggil method config() untuk memuat variabel lingkungan dari file .env
 dotenv.config();
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const corsOptions = {
+    origin:true,
+    credentials:true
+}
 
 const WisataRoute = require('./routes/WisataRoute');
 const UserRoute = require('./routes/UserRoute');
@@ -17,12 +23,20 @@ const PesananRoute = require('./routes/PesananRoute');
 const UlasanRoute = require('./routes/UlasanRoute');
 const AuthRoute = require('./routes/AuthRoute');
 
+app.use(cors(corsOptions))
+app.use(cookieParser())
 app.use('/wisata', WisataRoute);
 app.use('/user', UserRoute);
 app.use('/pusat-bantuan', PusatBantuanRoute);
 app.use('/pesanan', PesananRoute);
 app.use('/ulasan', UlasanRoute);
 app.use('/auth', AuthRoute);
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+}));
+
+app.use(cookieParser());
 
 server.listen(PORT, HOST, () => {
     console.log(`Server is running in http://localhost:${PORT}`);
--- frontend/src/component/admin/MainAdmin.jsx
+++ frontend/src/component/admin/MainAdmin.jsx
@@ -7,99 +7,24 @@ import withReactContent from 'sweetalert2-react-content'
 
 const MainAdmin = (props) => {
   const [page, setPage] = useState("admin")
-  const [datas, setDatas] = useState([
-    {
-        id: 1,
-        nama: 'Nusa Penida',
-        deskripsi: 'Nusa Penida adalah sebuah pulau yang ada di bali',
-        alamat: 'Klungkung',
-        harga: 150000,
-        jam_buka: '08.00',
-        jarak_lokasi: '20',
-        rating: '5',
-        galeri: [
-            'assets/img/bg2.png',
-            'assets/img/iamgeUser.png',
-            'assets/img/farm.png',
-        ],
-        informasi_tourguide: 'Percayakan wisata Anda kepada ahlinya! Kami akan memilihkan pemandu wisata terbaik di Nusa Penisa untuk menemani Anda. Tak perlu ragu lagi, Anda akan mendapatkan pengalaman wisata yang tak terlupakan dan penuh petualangan.',
-        rating_ulasan: 5
-    },
-    {
-        id: 2,
-        nama: 'Uluwatu Temple',
-        deskripsi: 'Pura Uluwatu adalah salah satu pura Hindu yang terkenal di Bali.',
-        alamat: 'Badung',
-        harga: 50000,
-        jam_buka: '09.00',
-        jarak_lokasi: '30',
-        rating: '4.5',
-        galeri: [
-            'assets/img/uluwatu1.png',
-            'assets/img/uluwatu2.png',
-            'assets/img/uluwatu3.png',
-        ],
-        informasi_tourguide: 'Pemandu wisata kami akan menjelaskan sejarah dan keindahan Pura Uluwatu dengan detail, membuat kunjungan Anda lebih bermakna.',
-        rating_ulasan: 4.5
-    },
-    {
-        id: 3,
-        nama: 'Tegallalang Rice Terrace',
-        deskripsi: 'Sawah terasering Tegallalang menawarkan pemandangan hijau yang memukau di Ubud.',
-        alamat: 'Gianyar',
-        harga: 25000,
-        jam_buka: '07.00',
-        jarak_lokasi: '15',
-        rating: '4.7',
-        galeri: [
-            'assets/img/tegallalang1.png',
-            'assets/img/tegallalang2.png',
-            'assets/img/tegallalang3.png',
-        ],
-        informasi_tourguide: 'Nikmati pemandangan dan pelajari cara bertani tradisional Bali dengan pemandu wisata kami.',
-        rating_ulasan: 4.7
-    },
-    {
-        id: 4,
-        nama: 'Monkey Forest',
-        deskripsi: 'Hutan Monyet Ubud adalah rumah bagi ratusan monyet ekor panjang.',
-        alamat: 'Ubud',
-        harga: 60000,
-        jam_buka: '08.00',
-        jarak_lokasi: '10',
-        rating: '4.3',
-        galeri: [
-            'assets/img/monkeyforest1.png',
-            'assets/img/monkeyforest2.png',
-            'assets/img/monkeyforest3.png',
-        ],
-        informasi_tourguide: 'Jelajahi hutan dan bertemu dengan monyet-monyet lucu dengan panduan dari ahli kami.',
-        rating_ulasan: 4.3
-    },
-    {
-        id: 5,
-        nama: 'Kuta Beach',
-        deskripsi: 'Pantai Kuta adalah salah satu pantai yang paling terkenal di Bali, cocok untuk berselancar.',
-        alamat: 'Kuta',
-        harga: 0,
-        jam_buka: '24 Jam',
-        jarak_lokasi: '5',
-        rating: '4.8',
-        galeri: [
-            'assets/img/kutabeach1.png',
-            'assets/img/kutabeach2.png',
-            'assets/img/kutabeach3.png',
-        ],
-        informasi_tourguide: 'Nikmati pantai dan ombak dengan bantuan instruktur selancar kami yang berpengalaman.',
-        rating_ulasan: 4.8
-    }
-  ])
+  const [datas, setDatas] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/wisata');
+        const fetchedDatas = await response.json();
+        setDatas([fetchedDatas]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
   useEffect(() => {
     if (props.tambahData != null) {
         setDatas((prevData) => [...prevData, props.tambahData])
     }
   }, [])
-  console.log(datas);
   const hapusData = (index) => {
     Swal.fire({
         title: "Apakah anda yakin?",
@@ -153,13 +78,14 @@ const MainAdmin = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {datas.map((data, index) => { 
-                        return <tr className="border-b border-slate-600" key={index}>
+                {datas.map((obj) => ( 
+                    obj.data.map((data, index) => (
+                        <tr className="border-b border-slate-600" key={index}>
                             <th scope="row" className="px-6 py-4 font-normal whitespace-nowrap bg-blue-300/30 text-center">
                                 {data.nama}
                             </th>
                             <td className="px-6 py-4 bg-blue-300/30 text-center">
-                            {data.deskripsi}
+                                {data.deskripsi}
                             </td>
                             <td className="px-6 py-4 bg-blue-300/30 text-center">
                                 {data.alamat}
@@ -168,11 +94,12 @@ const MainAdmin = (props) => {
                                 {data.harga}
                             </td>
                             <td className="px-6 py-4 bg-blue-300/30 text-center justify-center flex gap-5">
-                                <Link to = '/edit-wisata'><i className="fa-regular fa-pen-to-square text-green-600"></i></Link>
-                                <i class="fa-solid fa-square-minus text-red-600" onClick={() => hapusData(index)}></i>
+                                <Link to="/edit-wisata"><i className="fa-regular fa-pen-to-square text-green-600"></i></Link>
+                                <i className="fa-solid fa-square-minus text-red-600" onClick={() => hapusData(index)}></i>
                             </td>
                         </tr>
-                    })}
+                    ))
+                ))}
                 </tbody>
             </table>
         </div>
