59e857e922b2b57260615f39d03e166ed8953702
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-05-29 23:07:10 +0800

    Online Database

--- backend/api/src/config/database.js
+++ backend/api/src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const dbPool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tourguide_db',
+    host: 'SG-tour-guide-apps-8584-mysql-master.servers.mongodirector.com',
+    user: 'sgroot',
+    password: 'TssrFMTJs1Bf,Ow6',
+    database: 'nusaguide',
 })
 
 module.exports = dbPool.promise()
