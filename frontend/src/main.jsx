997a9c674d7024a62de42a73406939a7cae8bc08
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2024-06-13 10:07:52 +0700

    :memo: main.jsx auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
