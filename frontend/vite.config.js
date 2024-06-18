84ba7283a5c39e731352ca0d05fd83ebb6a5abdb
Wisnukan <28.ngakanmadewisnumahesaadnyana.com>
2024-06-18 13:29:49 +0800

    Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // pastikan ini sesuai dengan konfigurasi vercel.json
+    outDir: "frontend/dist", // pastikan ini sesuai dengan konfigurasi vercel.json
   },
 });
