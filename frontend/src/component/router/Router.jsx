4e282f61eb9286358ab80137414cf29a6398c944
melllonn07 <maya.anggita713@gmail.com>
2024-06-22 23:28:14 +0700

    add change password page

--- frontend/src/component/router/Router.jsx
+++ frontend/src/component/router/Router.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../../pages/home';
-import Package from '../../pages/Package';
-import PackageDetail from '../../pages/PackageDetail';
-import Login from '../../pages/Login';
-import Register from '../../pages/Register';
-import NotFound from '../../pages/NotFound';
-import Admin from '../../pages/Admin';
-import TambaData from '../admin/TambahData';
-import EditData from '../admin/EditData';
-import UserLayout from '../layout/userLayout';
-import Pembayaran from '../../pages/Pembayaran';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "../../pages/home";
+import Package from "../../pages/Package";
+import PackageDetail from "../../pages/PackageDetail";
+import Login from "../../pages/Login";
+import Register from "../../pages/Register";
+import NotFound from "../../pages/NotFound";
+import Admin from "../../pages/Admin";
+import TambaData from "../admin/TambahData";
+import EditData from "../admin/EditData";
+import UserLayout from "../layout/userLayout";
+import Pembayaran from "../../pages/Pembayaran";
 
-import ProfileUser from '../../pages/ProfileUser';
-import InvoicePage from '../../pages/InvoicePage';
-import BuktiPembayaran from '../../pages/BuktiPembayaranPage';
-import PembayaranPage from '../../pages/PembayaranPage';
-import RiwayatTransaksi from '../../pages/RiwayatTransaksiPage';
-import BantuanPage from '../../pages/BantuanPage';
+import ProfileUser from "../../pages/ProfileUser";
+import InvoicePage from "../../pages/InvoicePage";
+import BuktiPembayaran from "../../pages/BuktiPembayaranPage";
+import PembayaranPage from "../../pages/PembayaranPage";
+import RiwayatTransaksi from "../../pages/RiwayatTransaksiPage";
+import BantuanPage from "../../pages/BantuanPage";
+import ChangePassword from "../../pages/ChangePassword";
 
 const Router = () => {
   return (
@@ -42,6 +43,7 @@ const Router = () => {
           <Route path="riwayat-transaksi" element={<RiwayatTransaksi />} />
           <Route path="pusat-bantuan" element={<BantuanPage />} />
         </Route>
+        <Route path="change-password" element={<ChangePassword />} />
       </Routes>
     </>
   );
